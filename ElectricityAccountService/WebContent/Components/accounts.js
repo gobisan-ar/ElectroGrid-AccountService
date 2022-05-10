719a9a3f0f08b0f6caed81f879269bc812415157
Gobisan <Gobisan@192.168.1.6>
2022-05-10 19:50:34 +0530

    Add client-side component

--- ElectricityAccountService/WebContent/Components/accounts.js
+++ ElectricityAccountService/WebContent/Components/accounts.js
@@ -1,3 +1,159 @@
-/**
- * 
- */
+$(document).ready(function() 
+{ 
+	if ($("#alertSuccess").text().trim() == "") 
+	 { 
+	 	$("#alertSuccess").hide(); 
+	 } 
+	 $("#alertError").hide(); 
+});
+ 
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event) 
+{ 
+	// Clear alerts
+	 $("#alertSuccess").text(""); 
+	 $("#alertSuccess").hide(); 
+	 $("#alertError").text(""); 
+	 $("#alertError").hide(); 
+
+	// Form validation
+	var status = validateAccountForm(); 
+	if (status != true) 
+	 { 
+		 $("#alertError").text(status); 
+		 $("#alertError").show(); 
+		 return; 
+	 } 
+
+	// If valid
+	var type = ($("#hidAccountIDSave").val() == "") ? "POST" : "PUT";
+
+	$.ajax( 
+	{ 
+		 url : "AccountsAPI", 
+		 type : type, 
+		 data : $("#formAccount").serialize(), 
+		 dataType : "text", 
+		 complete : function(response, status) 
+		 { 
+		 	onAccountSaveComplete(response.responseText, status); 
+		 } 
+	});
+}); 
+
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function(event) 
+{ 
+	 $("#hidAccountIDSave").val($(this).data("accountid"));   
+	 $("#accName").val($(this).closest("tr").find('td:eq(0)').text()); 
+	 $("#accAddress").val($(this).closest("tr").find('td:eq(1)').text()); 
+	 $("#accPremise").val($(this).closest("tr").find('td:eq(2)').text()); 
+  	 $('input[name="rdoType"]:checked').val($(this).closest("tr").find('td:eq(3)').text()); 
+  	 $("#ddlPurpose").val($(this).closest("tr").find('td:eq(4)').text()); 
+  	 $("#ddlSuply").val($(this).closest("tr").find('td:eq(5)').text()); 
+  	 $('input[name="rdoStatus"]:checked').val($(this).closest("tr").find('td:eq(5)').text()); 
+  	 
+}); 
+
+$(document).on("click", ".btnRemove", function(event) 
+{ 
+	 $.ajax( 
+	 { 
+		 url : "AccountsAPI", 
+		 type : "DELETE", 
+		 data : "accountID=" + $(this).data("accountid"),
+		 dataType : "text", 
+		 complete : function(response, status) 
+		 { 
+		 	onAccountDeleteComplete(response.responseText, status); 
+		 } 
+	 }); 
+});
+	
+// CLIENT-MODEL================================================================
+function validateAccountForm() 
+{ 
+	if ($("#accName").val().trim() == "") 
+	 { 
+	 	return "Insert Account Name."; 
+	 } 
+
+	if ($("#accAddress").val().trim() == "") 
+	 { 
+	 	return "Insert Billing Address."; 
+	 }
+
+	if ($("#accPremise").val().trim() == "") 
+	 { 
+	 	return "Insert Connection Premise."; 
+	 } 
+	 
+	 if ($('input[name="rdoType"]:checked').length === 0) 
+	 { 
+	 	return "Select Connection Type."; 
+	 } 
+
+	if ($("#ddlPurpose").val() == "0") 
+	 { 
+	 	return "Select usage purpose."; 
+	 } 
+
+	return true; 
+}
+
+function onAccountSaveComplete(response, status) 
+{ 
+	if (status == "success") 
+	 { 
+	 	var resultSet = JSON.parse(response); 
+	 	
+	 	if (resultSet.status.trim() == "success") 
+		 	{ 
+		 $("#alertSuccess").text("Successfully saved."); 
+		 $("#alertSuccess").show(); 
+		 $("#divAccountsGrid").html(resultSet.data); 
+	 	} else if (resultSet.status.trim() == "error") 
+	 	{ 
+			 $("#alertError").text(resultSet.data); 
+			 $("#alertError").show(); 
+	 	} 
+	 } else if (status == "error") 
+	 { 
+		 $("#alertError").text("Error while saving."); 
+		 $("#alertError").show(); 
+	 } else
+	 { 
+		 $("#alertError").text("Unknown error while saving.."); 
+		 $("#alertError").show(); 
+	 }
+	 $("#hidAccountSave").val(""); 
+	 $("#formAccount")[0].reset(); 
+}
+
+function onAccountDeleteComplete(response, status) 
+{ 
+	if (status == "success") 
+	 { 
+		 var resultSet = JSON.parse(response); 
+		 
+		 if (resultSet.status.trim() == "success") 
+		 { 
+		 	$("#alertSuccess").text("Successfully deleted."); 
+		 	$("#alertSuccess").show(); 
+		 	
+		 	$("#divAccountsGrid").html(resultSet.data); 
+		 } else if (resultSet.status.trim() == "error") 
+		 { 
+		 	$("#alertError").text(resultSet.data); 
+		 	$("#alertError").show(); 
+		 } 
+	 } else if (status == "error") 
+	 { 
+	 	$("#alertError").text("Error while deleting."); 
+	 	$("#alertError").show(); 
+	 } else
+	 { 
+	 	$("#alertError").text("Unknown error while deleting.."); 
+	 	$("#alertError").show(); 
+	 } 
+}
--- ElectricityAccountService/WebContent/accounts.jsp
+++ ElectricityAccountService/WebContent/accounts.jsp
@@ -1,12 +1,106 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"
+	import="com.electricityaccount.model.ElectricityAccountLogic"%>
+
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ElectroGrid</title>
+<link rel="stylesheet" href="Views/bootstrap.min.css">
+<script src="Components/jquery-3.6.0.min.js"></script>
+<script src="Components/accounts.js"></script>
 </head>
 <body>
+	<div class="container">
+		<div class="row">
+			<div class="col">
 
+				<h1>Electricity Accounts Management</h1>
+				<br>
+
+				<form id="formAccount" name="formAccount" method="post"
+					action="accounts.jsp">
+
+					<label>Account name: </label>
+					<input id="accName" name="accName" type="text" class="form-control form-control-sm"> 
+					<br> 
+					
+					<label>Billing Address:</label> 
+					<input id="accAddress" name="accAddress" type="text" class="form-control form-control-sm"> 
+					<br>
+					
+					<label>Connection Premise: </label>
+					<input id="accPremise" name="accPremise" type="text" class="form-control form-control-sm"> 
+					<br>
+					
+					<label>Connection Type:</label>
+					&nbsp;&nbsp;
+					
+					Permanent
+					<input type="radio" class="conType" id="rdoTypePermanet" name="rdoType" value="Permanent"> 
+					&nbsp;&nbsp; 
+					
+					Temporary 
+					<input type="radio" class="conType" id="rdoTypeTemporary" name="rdoType" value="Temporary"> 
+					<br><br>
+					
+					<label>Purpose:</label>
+					&nbsp;&nbsp;
+					<select id="ddlPurpose" name="ddlPurpose">
+							<option value="0">--Purpose of Electricity usage--</option>
+							<option value="Domestic">Domestic</option>
+							<option value="Shop">Shop</option>
+							<option value="Factory">Factory</option>
+							<option value="Hotel">Hotel</option>
+							<option value="Other">Other</option>
+					</select>
+					<br><br>
+					
+					<label>Electricity Supply:</label>
+					&nbsp;&nbsp;
+					<select id="ddlSuply" name="ddlSuply">
+							<option value="0">--Electricity Supply--</option>
+							<option value="15">Single Phase 15A</option>
+							<option value="30">Single Phase 30A</option>
+							<option value="60">Three Phase 60A</option>
+					</select>
+					<br><br>
+					
+					<label>Status:</label>
+					&nbsp;&nbsp;
+					
+					Active
+					<input type="radio" class="conStatus" id="rdoStatusActive" name="rdoStatus" value="Active"> 
+					&nbsp;&nbsp; 
+					
+					Disconnected 
+					<input type="radio" class="conStatus" id="rdoStatusDiscon" name="rdoStatus" value="Disconnected"> 
+					<br><br>
+					
+					<br>
+					<input id="btnSave" name="btnSave" type="button" value="Save" class="btn btn-primary"> 
+					
+					<input type="hidden" id="hidAccountIDSave" name="hidAccountIDSave" value="">
+				</form>
+
+				<br>
+
+				<div class="alert alert-success">
+					<%
+					out.print(session.getAttribute("statusMsg"));
+					%>
+				</div>
+				<br> <br>
+
+				<%
+					ElectricityAccountLogic eacc = new ElectricityAccountLogic();
+					out.print(eacc.getAllElectricityAccounts());
+				%>
+
+			</div>
+		</div>
+	</div>
 </body>
 </html>
